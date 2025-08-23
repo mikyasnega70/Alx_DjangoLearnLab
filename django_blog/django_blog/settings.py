2bad75fbdcba4575480f80c5464310aa42bbf4d3
mikyasnega70 <mikenega70@gmail.com>
2025-08-23 22:45:21 +0300

    project

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog_db',
+        'USER': 'your_db_user',         
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '3306',                 
     }
 }
 
